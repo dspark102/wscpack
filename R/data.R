e9a40b172df6c4f92714356facc7acab4c9013d1
Baek <qkr800507@gmail.com>
2024-05-25 00:22:32 -0400

    finale

--- R/data.R
+++ R/data.R
@@ -1,12 +1,23 @@
-#' WSC
+#' WSC Dataset
 #'
-#' A dataset containing XYZ information.
+#' A dataset containing 2200 observations and 44 columbns used for WSC four arm study.
 #'
 #' @format A data frame with X rows and Y variables:
 #' \describe{
-#'   \item{var1}{Description of var1}
-#'   \item{var2}{Description of var2}
+#'   \item{female}{Responder's gender is female}
+#'   \item{white}{Responder's race is White}
+#'   ...
+#'   \item{logBDI}{log of BDI scale }
 #'
 #' }
 #' @source \url{https://example.com}
+#'
+#' {B.Keller et al. (2024)}....
+#'
+#' @examples
+#' # example code
+#'
+#'
+#'
+#'
 "my_data"
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# wscpack
+# WSC pack
 
 <!-- badges: start -->
 <!-- badges: end -->
