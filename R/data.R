0263c595a0c7d83f0b91b66f507f6ba0563a8604
Baek <qkr800507@gmail.com>
2024-06-04 15:17:14 -0400

    new documentation

--- R/data.R
+++ R/data.R
@@ -53,7 +53,7 @@
 #' Experimental and quasi-experimental identification of conditional average treatment effects:
 #' A four-arm within-study comparison. OSF. Retrieved from \url{https://osf.io/e3mpf}
 #'
-#' @source  data
+#' @source paper ... (tbd)
 #' @examples
 #' \dontrun{
 #'   data <- read.csv("data2200.csv")
